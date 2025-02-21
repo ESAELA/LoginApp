ffce46de57466f1da519bd0aebe30b287ad9682f
ESAELA <esamuelchinama@hotmail.com>
2025-02-21 11:15:36 +0200

    up

--- src/pages/Recovery.jsx
+++ src/pages/Recovery.jsx
@@ -2,20 +2,23 @@ function Recovery () {
     return (
         <div className="recovery-bgcolor h-screen w-screen flex justify-center items-center">
             
-            <form className="form-components p-10 flex flex-col gap-10">
+            <form className="form-component s p-10 flex flex-col gap-10 bg-amber-600">
+
+                <div className="flex flex-col gap-10 justify-center items-center">
+                    <div className="form-primarydiv bg-red-200">
+                        <h3 className="">
+                            Não consegue entrar?
+                        </h3>
+                    </div>
+                    <div className="form-secondarydiv bg-amber-500">
+                        <p className="text-center"> 
+                            Há alguns motivos pelos quais você pode não conseguir fazer login. Verifique as opções abaixo para possíveis soluções.
+                        </p>
+                    </div>
+                </div>
                 
-                <div className="flex justify-center">
-                    <h3 className="text-center form-primarydiv">
-                        Não consegue entrar?
-                    </h3>
-                </div>
-                <div className="flex justify-center">
-                    <p id='paragrado-secondary' className="text-center form-secondarydiv"> 
-                        Há alguns motivos pelos quais você pode não conseguir fazer login. Verifique as opções abaixo para possíveis soluções.
-                    </p>
-                </div>
     
-                <div className="flex justify-center gap-10">
+                <div className="flex flex-row justify-center gap-10">
                     <div className="recovery-divs">
                         <h2>
                             Esqueçeu o seu nome do úsuario?
@@ -33,6 +36,7 @@ function Recovery () {
                         </p>
                     </div>
                 </div>
+                
             </form>
         </div>
     ) 
